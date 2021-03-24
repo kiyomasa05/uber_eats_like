49d35a25779935f23ecc68d75a7ff48d2086285b
kiyomasa05 <m230549@gmail.com>
2021-03-24 17:55:34 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do #これはコントローラーをグルーピング,URLにもその情報を付与する
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods,only: %i[index]#Restaurant一覧/api/v1/restaurants #Food一覧/api/v1/restaurants/:restaurant_id/foodsを取得
+      end
+      resources :line_foods, only: %i[index create]
+      put 'line_foods/replace', to: 'line_foods#replace'
+      resources :orders, only: %i[create]
+    end
+  end
 end
